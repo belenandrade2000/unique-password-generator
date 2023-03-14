b782000bd151ba99b6283a1f442ba8126e34aa04
Belen Andrade <belenandrade2000@gmail.com>
2023-03-13 19:19:12 -0700

    added README with screenshot + function to put prompt again when user fails to put enough characters

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # unique-password-generator
+
+## Description
+
+This webpage is a password generator where a user can answer some prompts and the computer will create a password for the user. Sometimes making a password on your own can be tiring and fitting in all the password requirements can take a while. This password generator will generate a password between 8-128 characters with special characters, numbers, uppercase and lowercase letters. By simply clicking on the "generate password" button, the user can answer a few quick questions to make the password the user wants. With this project, I learned how to use javascript to randomize array elements and concat them.
+
+## Table of Contents
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+When a user gets on the password generator webpage, they will click on the "generate password" button. Once clicked, a prompt at the top will pop out. The user will then be instructed to enter a number 8-128 and then click enter. If user does not select a number 8-128, they will be prompted to try again. There will be 4 other prompts that pop up that the user can "ok" on or "cancel" on. These 4 prompts are...
+1. Click OK to confirm including special characters.
+2. Click OK to confirm including numeric characters.
+3. Click OK to confirm including lowercase characters.
+4. Click OK to confirm including uppercase characters.
+
+Once the user answers all prompts, the webpage will display their new unique password with all the criteria they chose. The user must click "ok" to at least 1 prompt to be able to generate a password. If the user does not accept at least 1 criteria, the browser will alert the user that at least 1 criteria must be selected and to try again.
+
+The below example shows an example of how the password will be presented to the user after answering the prompts. This particular example is of a password with 20 characters and with all criteria available.
+
+![image](./images/browser.png)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the Github repository.
--- script.js
+++ script.js
@@ -5,13 +5,16 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
+
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//character arrays
 var specialCharac = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "^", "_", "`", "{", "|", "}", "~", "]"]
+
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -20,32 +23,32 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
   'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
   'Y', 'Z']
 
-
-function generatePassword() {
-  var charactersLength = prompt("How many characters would you like your password to contain?");
+var charactersLength;
+function characters() {
+  charactersLength = prompt("How many characters would you like your password to contain?");
   if (charactersLength < 8) {
-    ;
-    alert("Password length must be at least 8 characters. Please input again")
-    characters(); // not letting me input again
+    alert("Password length must be at least 8 characters. Please input again");
+    characters();
   } else if (charactersLength > 128) {
-    ;
-    alert("Password length must not exceed 128 characters. Please input again")
+    alert("Password length must not exceed 128 characters. Please input again");
     characters();
   } else if (!charactersLength) {
-    alert("Invalid input. Please provide a number 8-128.")
-  } else {
-    ;
-    var confirmSpecial = confirm("Click OK to confirm including special characters");
-    var confirmNum = confirm("Click OK to confirm including numeric characters");
-    var confirmLowerCase = confirm("Click OK to confirm including lowercase characters");
-    var confirmUpper = confirm("Click OK to confirm including uppercase characters");
-};
+    alert("Invalid input. Please provide a number 8-128.");
+    characters();
+  }
+}
+function generatePassword() {
+  characters();
+  var confirmSpecial = confirm("Click OK to confirm including special characters");
+  var confirmNum = confirm("Click OK to confirm including numeric characters");
+  var confirmLowerCase = confirm("Click OK to confirm including lowercase characters");
+  var confirmUpper = confirm("Click OK to confirm including uppercase characters");
 
   // Selecting all arrays
   if (confirmSpecial && confirmNum && confirmLowerCase && confirmUpper) {
     var userSelection = specialCharac.concat(numbers, lowerCase, upperCase);
 
-// Selecting 3 arrays
+    // Selecting 3 arrays
   } else if (!confirmSpecial && confirmNum && confirmLowerCase && confirmUpper) {
     var userSelection = numbers.concat(lowerCase, upperCase);
   } else if (confirmSpecial && !confirmNum && confirmLowerCase && confirmUpper) {
@@ -57,55 +60,55 @@ function generatePassword() {
   }
 
   // Selecting 2 arrays
-  else if (confirmSpecial && confirmNum){
-  var userSelection = specialCharac.concat(numbers);
+  else if (confirmSpecial && confirmNum) {
+    var userSelection = specialCharac.concat(numbers);
   }
-  else if (confirmSpecial && confirmLowerCase){
-  var userSelection = specialCharac.concat(lowerCase);
+  else if (confirmSpecial && confirmLowerCase) {
+    var userSelection = specialCharac.concat(lowerCase);
   }
-  else if (confirmSpecial && confirmUpper){
-  var userSelection = specialCharac.concat(upperCase);
+  else if (confirmSpecial && confirmUpper) {
+    var userSelection = specialCharac.concat(upperCase);
   }
-  else if (confirmNum && confirmLowerCase){
-  var userSelection = numbers.concat(lowerCase);
+  else if (confirmNum && confirmLowerCase) {
+    var userSelection = numbers.concat(lowerCase);
   }
-  else if (confirmNum && confirmUpper){
-  var userSelection = numbers.concat(upperCase);
+  else if (confirmNum && confirmUpper) {
+    var userSelection = numbers.concat(upperCase);
   }
-  else if (confirmLowerCase && confirmUpper){
-  var userSelection = lowerCase.concat(upperCase);
+  else if (confirmLowerCase && confirmUpper) {
+    var userSelection = lowerCase.concat(upperCase);
   }
 
-   // Selecting 1 array
+  // Selecting 1 array
   else if (confirmSpecial) {
-  var userSelection = specialCharac;
-  }else if (confirmNum){
-  var userSelection = numbers;
-  }else if (confirmLowerCase){
-  var userSelection = lowerCase;
-  }else if (confirmUpper){
-  var userSelection = upperCase;
+    var userSelection = specialCharac;
+  } else if (confirmNum) {
+    var userSelection = numbers;
+  } else if (confirmLowerCase) {
+    var userSelection = lowerCase;
+  } else if (confirmUpper) {
+    var userSelection = upperCase;
   }
 
   //Selecting 0 array
   else {
-    alert("You must choose at least 1 criteria. Please try again")
+    alert("You must choose at least 1 criteria. Please try again");
   }
 
 
-// generate password
-var emptyPassword = [];
-for (var i = 0; i < charactersLength; i++) {
-  var alluserSelection = userSelection[Math.floor(Math.random() * userSelection.length)];
-  emptyPassword.push(alluserSelection); }
-
-var password = emptyPassword.join("");
-console.log(password)
-
-    return password
+  // generate password
+  var emptyPassword = [];
+  for (var i = 0; i < charactersLength; i++) {
+    var alluserSelection = userSelection[Math.floor(Math.random() * userSelection.length)];
+    emptyPassword.push(alluserSelection);
   }
+  var password = emptyPassword.join("");
+  console.log(password);
+
+  return password;
+}
 
 
 
-  // Add event listener to generate button
-  
+
+
